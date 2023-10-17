ff26b248bf6b35251f996b5e76051eb35d7c0429
Yashi Lin <yashilin19@gmail.com>
2023-10-17 13:54:45 +1300

    update icon

--- src/components/solar.jsx
+++ src/components/solar.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import "./spacejam.css";
 import DisplayPanel from "./PanelData";
 import drumSounds from "./presets/drumSounds";
+import questionMarkIcon from "/icon/icon-question.svg"; 
 
 const gap = 49;
 const radius = 74; // radius of the orbit of the closest period
@@ -222,19 +223,7 @@ function Solar({
           </div>
             <div className=" ">
               <span className="inline-block mr-2">
-                <svg
-                  className="icon"
-                  viewBox="0 0 1024 1024"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="14"
-                  height="14"
-                >
-                  <path
-                    d="M532.042 0C372.404 0 269.006 65.406 187.828 182.052c-14.726 21.16-10.186 50.172 10.356 65.748l86.276 65.418c20.746 15.73 50.264 12.052 66.506-8.296 50.098-62.762 87.26-98.898 165.514-98.898 61.528 0 137.632 39.598 137.632 99.262 0 45.104-37.234 68.268-97.986 102.328-70.846 39.72-164.598 89.152-164.598 212.81V640c0 26.51 21.49 48 48 48h144.942c26.51 0 48-21.49 48-48v-11.546c0-85.72 250.536-89.29 250.536-321.254C883.008 132.512 701.804 0 532.042 0zM512 746.918c-76.392 0-138.542 62.15-138.542 138.542 0 76.39 62.15 138.54 138.542 138.54s138.542-62.15 138.542-138.542-62.15-138.54-138.542-138.54z"
-                    fill="#5bba74"
-                  ></path>
-                </svg>
+              <img src={questionMarkIcon} alt="Question Mark" width="14" height="14" />
               </span>
               <span className="text-white ">Refresh Time (ms): </span>
               <input
